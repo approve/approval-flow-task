434b6c4b865605b3f007ca8e40d2a0d1e58fc4d1
Sergey Bolshchikov <sergey@bolshchikov.net>
2021-06-03 13:18:23 +0300

    refactor server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-
-const { buildApprovalFlow } = require('./approval-flow-builder');
+const { buildApprovalFlow } = require('./approval-flow');
 
 const app = express();
 const port = 3001;
 
-app.get('/', buildApprovalFlow)
+app.get('/_api/approval-flow', buildApprovalFlow)
 
 app.listen(port, () => {
     console.log(`app listening on http://localhost:${port}`)
